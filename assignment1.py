78bc268607da8520f5caa869e475a28b7783a4d8
DGMGIT <daniel.mackenzie1@my.jcu.edu.au>
2020-09-07 21:11:21 +1000

    Travel Tracker A1 Name: Daniel Mackenzie Date started: 30/08/20 Draft 2 - part 00 base program and files

---
+++ assignment1.py
@@ -0,0 +1,16 @@
+"""
+Travel Tracker A1
+Name: Daniel Mackenzie
+Date started: 30/08/20
+GitHub URL:
+Draft 2 - part 00
+base program
+"""
+
+
+def main():
+    print("Travel Tracker 1.0 - by <Your Name>")
+
+
+if __name__ == '__main__':
+    main()
