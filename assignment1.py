3230842141872ba12b62dc309a04cd42dff87e9f
DGMGIT <daniel.mackenzie1@my.jcu.edu.au>
2020-09-07 21:20:40 +1000

    Travel Tracker A1 Draft 2 - part 01 worked on program mean system added

--- assignment1.py
+++ assignment1.py
@@ -2,14 +2,26 @@
 Travel Tracker A1
 Name: Daniel Mackenzie
 Date started: 30/08/20
-GitHub URL:
-Draft 2 - part 00
-base program
+GitHub URL: https://github.com/DGMGIT/travel-tracker-assignment-1-ichaturvedi-master
+Draft 2 - part 01
+worked on program mean system added
 """
+
+
+MENU = "Menu:\nL - List places\nA - Add new place\nM - Mark a place as visited\nQ - Quit\n>>> "
 
 
 def main():
     print("Travel Tracker 1.0 - by <Your Name>")
+    while get_choices != "Q":
+        if get_choices == "L":
+            pass
+        elif get_choices == "A":
+            pass
+        elif get_choices == "M":
+            pass
+        else:
+            pass
 
 
 if __name__ == '__main__':
